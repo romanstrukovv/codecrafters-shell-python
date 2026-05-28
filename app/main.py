a79f10d710fac6678d93213a878a4106e8a88261
romanstrukovv <romanstrukovv@gmail.com>
2026-05-27 19:42:36 -0500

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,9 +2,12 @@ import sys
 
 
 def main():
-    # TODO: Uncomment the code below to pass the first stage
-    sys.stdout.write("$ ")
-    pass
+    # # TODO: Uncomment the code below to pass the first stage
+    # sys.stdout.write("$ ")
+    # pass
+
+    command = input("$ ")
+    print(f'{command} not found')
 
 
 if __name__ == "__main__":
