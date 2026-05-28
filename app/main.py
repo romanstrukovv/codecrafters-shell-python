f7646ababdc1e1ea6cdb87f71a33c07c1b2a86fe
romanstrukovv <romanstrukovv@gmail.com>
2026-05-27 20:15:46 -0500

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -17,7 +17,7 @@ def main():
             print(" ".join(args))
         elif cmd == "type": 
             if args and args[0] in valid_cmds:
-                print(f'{command} is a shell builtin')
+                print(f'{args[0]} is a shell builtin')
             else:
                 print(f'{command}: command not found')           
         else:
